7ea481e76f3afb6d4b1a89be7a05ae424e185b86
Leeji0 <dldmsgktjf0716@gmail.com>
2024-08-01 17:36:23 +0900

    test

---
+++ front/my-nextjs-app/src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  
+  //리턴
+  return (
+    <div>
+      <link href="https://cdn.jsdelivr.net/npm/daisyui@2.13.6/dist/full.css" rel="stylesheet" type="text/css" />
+      <script src="https://cdn.tailwindcss.com"></script>
+      <div id="root">
+
+      </div>
+      <div className="flex justify-center">
+        이지영 바보
+      </div>
+    </div>
+  );
+}
+
